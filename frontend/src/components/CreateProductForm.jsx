469fdb9674b7cddb3967b00a587695fd29d6976a
leviatancode <gabbodlc03@gmail.com>
2025-01-13 17:35:25 +0100

    update

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { PlusCircle, Upload, Loader } from "lucide-react";
 import { useProductStore } from "../stores/useProductStore";
 
-const categories = ["jeans", "t-shirts", "shoes", "glasses", "jackets", "suits", "bags"];
+const categories = ["CPU", "GPU", "Cases", "Motherboards", "Accessories", "Power Supplies", "Ram", "Storage"];
 
 const CreateProductForm = () => {
 	const [newProduct, setNewProduct] = useState({
